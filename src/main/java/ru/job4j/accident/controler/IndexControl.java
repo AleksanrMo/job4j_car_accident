ef1f500a56a78ae98165efa04b3d3f4fda91ecee
AleksandrMo <sushanta@mail.ru>
2022-03-24 14:19:24 +0700

    commit

--- src/main/java/ru/job4j/accident/controler/IndexControl.java
+++ src/main/java/ru/job4j/accident/controler/IndexControl.java
@@ -4,10 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
-        return "info";
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        model.addAttribute("list", list);
+        return "index";
     }
 }
