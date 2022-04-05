a4392b6a408c18d54be621fa55a8e75247665347
AleksandrMo <sushanta@mail.ru>
2022-04-05 20:18:24 +0700

    commit

--- src/main/java/ru/job4j/accident/config/SecurityConfig.java
+++ src/main/java/ru/job4j/accident/config/SecurityConfig.java
@@ -1,12 +1,9 @@
 package ru.job4j.accident.config;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,8 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-
 import javax.sql.DataSource;
 
 
@@ -23,19 +18,17 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-   DataSource ds;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(ds)
-                .withUser(User.withUsername("user")
-                        .password(passwordEncoder().encode("123456"))
-                        .roles("USER"));
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user").password(passwordEncoder.encode("123456")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
     }
-
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -64,4 +57,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable();
     }
+
 }
+//    @Autowired
+//    PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    DataSource ds;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(ds)
+//                .withUser(User.withUsername("user")
+//                        .password(passwordEncoder().encode("123456"))
+//                        .roles("USER"));
+//    }
+//
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/login")
+//                .permitAll()
+//                .antMatchers("/**")
+//                .hasAnyRole("ADMIN", "USER")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login?error=true")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/login?logout=true")
+//                .invalidateHttpSession(true)
+//                .permitAll()
+//                .and()
+//                .csrf()
+//                .disable();
+//    }
